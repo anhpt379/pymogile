06004e38697d7bacf2cc95d4e45bbcf759803b0c
Phạm Tuấn Anh <tuananhpham@vccorp.vn>
2017-12-20 12:00:13 +0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pymogile',
-    version='2.0.2',
+    version='2.1.0',
     description="pymogile",
     long_description="""Python MogileFS Client""",
     classifiers=(
